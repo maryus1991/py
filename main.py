12d39f58236a61e590f2d84acf5a00c578070f31
maryus1991 <maryus19915123@gmail.com>
2021-09-26 15:10:22 +0330

    fix audio_translator

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 from moviepy.editor import AudioFileClip
-from pydub import AudioSegment
-# import readline as sr
-# sound = sr.Recognizer()
+from pydub import AudioSegment, playback
+import speech_recognition as sr
 
 def video_getter_audio_path_return(path):
     audio_of_video = AudioFileClip(path)
@@ -12,11 +11,20 @@ def video_getter_audio_path_return(path):
     audio_of_video.write_audiofile(audio_path)
     return audio_path
 
+
 # create audio_slicer func
-def audio_slicer(audio_path):
+
+def audio_slicer(audio_path, start_second, end_second):
     audio_clip = AudioSegment.from_mp3(audio_path)
-    sliced_part = (len(audio_clip) / 1000) / 5
+    sliced_part_of_audio = audio_clip[start_second:end_second + 150 ]
+    audio_data = sliced_part_of_audio.raw_data
+    return audio_data
 
-    print(sliced_part)
+def audio_translator(audio_data):
+    r = sr.Recognizer()
+    audio_data_one = sr.AudioData(audio_data, 441, 1)
+    audio_data_two = r.record(audio_data_one.get_raw_data)
+    text = r.recognize_google(audio_data_two)
+    print(text)
 
-audio_slicer(r'/home/maryus/Desktop/nothing/py/subtitle maker/media/Python_WiFi_DoS_Denial_of_Service_attack.136.mp4.mp3')
+audio_translator(audio_slicer('media/Python_WiFi_DoS_Denial_of_Service_attack.136.mp4.mp3',0 ,6 ))
