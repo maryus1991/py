95c28ed740d766e56119e2039d62d69337c24cee
maryus1991 <maryus19915123@gmail.com>
2021-09-26 20:14:31 +0330

    add subtitle_file_writer and fix audio_translator

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from moviepy.editor import AudioFileClip
 from pydub import AudioSegment, playback
 import speech_recognition as sr
+import io
 
 def video_getter_audio_path_return(path):
     audio_of_video = AudioFileClip(path)
@@ -20,11 +21,32 @@ def audio_slicer(audio_path, start_second, end_second):
     audio_data = sliced_part_of_audio.raw_data
     return audio_data
 
-def audio_translator(audio_data):
-    r = sr.Recognizer()
-    audio_data_one = sr.AudioData(audio_data, 441, 1)
-    audio_data_two = r.record(audio_data_one.get_raw_data)
-    text = r.recognize_google(audio_data_two)
-    print(text)
+def audio_translator(audio_data):   
+    # print(audio_data)
+    audio_sound_data = AudioSegment(audio_data,  sample_width=2, frame_rate=44100, channels=2)
+    translator = sr.Recognizer()
+    with sr.AudioFile(audio_sound_data.export(format='wav')) as audio_source:
+    # listen for the data (load audio to memory)
+        audio_data = translator.record(audio_source)
+        # recognize (convert from speech to text)
+        translated_sound_as_string = translator.recognize_google(audio_data)
+        return translated_sound_as_string
 
-audio_translator(audio_slicer('media/Python_WiFi_DoS_Denial_of_Service_attack.136.mp4.mp3',0 ,6 ))
+def subtitle_file_writer(text, endtime, starttime , filename:str):
+    try :
+        subtitle_file = open(filename + '.srt', 'a')
+    except :
+        subtitle_file = open(filename + '.srt', 'w')
+
+    result_text = f'{starttime} --> {endtime}\n {text}' 
+    subtitle_file.write(result_text)
+    subtitle_file.close()
+
+
+         
+
+
+subtitle_file_writer(audio_translator
+                    (audio_slicer
+                                ('media/Python_WiFi_DoS_Denial_of_Service_attack.136.mp4.mp3',0 ,2500 )), '00:00:25', '00:00:25',
+                                    'example' )
